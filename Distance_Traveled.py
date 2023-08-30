c09748d935ff2902d6553ddaf069f2597c3afc76
MilesJones18 <gameghostjones@gmail.com>
2023-08-30 15:32:23 -0500

    More excercise

---
+++ Distance_Traveled.py
@@ -0,0 +1,5 @@
+speed = 70
+
+print(f"The distance the car will travel in 6 hours is: {speed * 6} miles.")
+print(f"The distance the car will travel in 10 hours is: {speed * 10} miles.")
+print(f"The distance the car will travel in 15 hours is: {speed * 15} miles.")
