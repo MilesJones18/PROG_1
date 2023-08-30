6d5eed92ccb97eb99a4539992023f3e52c24e82b
MilesJones18 <gameghostjones@gmail.com>
2023-08-29 23:23:39 -0500

    Need to finish

---
+++ Miles_Jones_challenge_02.py
@@ -0,0 +1,8 @@
+#user_name = input("What is your name? ")
+day = int(input("Please enter your date of birth (Day, Month, Year): "))
+
+
+
+
+print(day)
+#print(f"Hello {user_name}. According to your birthday, you are {age} years old.")
