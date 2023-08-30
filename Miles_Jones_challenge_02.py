aae8ca4c310f595685c168f620753225edfbf324
MilesJones18 <gameghostjones@gmail.com>
2023-08-30 00:42:26 -0500

    Completely working non.

--- Miles_Jones_challenge_02.py
+++ Miles_Jones_challenge_02.py
@@ -1,8 +1,7 @@
 user_name = input("What is your name? ")
 date = str(input("Please enter your date of birth (Day/Month/Year): "))
-   
-user_year = int(date[6:10])  # Takes the date string that the user inputs slices the year out and turns it into an integer. 
-age = 2023 - user_year
 
+foo = date.split("/")  # I wanted a more elegant way to get the year separate from the user inputted string. Split turns the string into a list of day, month, and year.  
 
+age = 2023 - int(foo[2])
 print(f"Hello {user_name}. According to your birthday, you are {age} years old.")
