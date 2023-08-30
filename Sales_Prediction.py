1f04ecb642ee0bb11509a618f74713a932d1e687
MilesJones18 <gameghostjones@gmail.com>
2023-08-30 15:27:31 -0500

    Simple excercise

---
+++ Sales_Prediction.py
@@ -0,0 +1,7 @@
+total_sales = int(input("What is your projected amount of total sales?: "))
+
+annual_profit = 0.23
+
+total_profit = total_sales * annual_profit
+
+print(f"The total profit made is: ${total_profit}")
