eb2d1a69ed1511914202649420aac98aa272fd93
MilesJones18 <gameghostjones@gmail.com>
2023-08-29 23:55:54 -0500

    updated a little, works now.

--- Miles_Jones_challenge_02.py
+++ Miles_Jones_challenge_02.py
@@ -1,8 +1,8 @@
-#user_name = input("What is your name? ")
-day = int(input("Please enter your date of birth (Day, Month, Year): "))
+user_name = input("What is your name? ")
+date = str(input("Please enter your date of birth (Day/Month/Year): "))
+   
+user_year = int(date[6:10])  # Takes the date string that the user inputs slices the year out and turns it into an integer. 
+age = 2023 - user_year
 
 
-
-
-print(day)
-#print(f"Hello {user_name}. According to your birthday, you are {age} years old.")
+print(f"Hello {user_name}. According to your birthday, you are {age} years old.")
