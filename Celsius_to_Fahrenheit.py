fd4f8ff692dd52ab432bd2d0db2aee471379553b
MilesJones18 <gameghostjones@gmail.com>
2023-09-02 01:36:14 -0500

    More excercise

---
+++ Celsius_to_Fahrenheit.py
@@ -0,0 +1,5 @@
+celsius = float(input("Please enter the current temperature in celsius: "))
+
+fahrenheit = float(((9/5) * celsius) + 32)
+
+print(f"The temperature in fahrenheit is: {fahrenheit}")
---
+++ Miles-per-gallon.py
@@ -0,0 +1,7 @@
+miles_driven  = float(input("Please enter the amount of miles driven: "))
+gallons_used = float(input("Please enter the gallons used: "))
+
+
+MPG = miles_driven / gallons_used
+
+print(f"Your MPG is: {MPG}")
