d1d57f1e9b87da7f43bd456a46b2fab92164d590
MilesJones18 <gameghostjones@gmail.com>
2023-09-03 21:22:05 -0500

    Stock_Transaction

--- Stock_Transaction.py
+++ Stock_Transaction.py
@@ -1,6 +1,34 @@
-stock_commission_bought = float(input())
-stock_commission_sold
-number_of_shares_purchased
-share_price
-number_of_shares_sold
-share_sold
+print(f"***** Welcome to the stock profit calculator ***** \n" )
+stock_commission_bought = float(input("Enter the commission for the bought shares: "))
+number_of_shares_purchased = int(input("Enter the number of shares purchased: "))
+share_price =  float(input("Enter the buy price per share: "))
+stock_commission_sold = float(input("Enter the commission percentage for the sold shares: "))
+number_of_shares_sold = int(input("Enter the number of shares sold: "))
+share_sold = float(input("Enter the sell price per share: "))
+
+
+price_paid = number_of_shares_purchased * share_price
+commission_bought = stock_commission_bought / 100
+commission_bought_total = price_paid * commission_bought
+price_paid = number_of_shares_purchased * share_price + commission_bought_total
+
+
+price_sold = number_of_shares_sold * share_sold
+commission_sold = stock_commission_sold / 100
+commission_sold_total = price_sold * commission_sold
+price_sold = number_of_shares_sold * share_sold + commission_sold_total
+
+
+total_paid = price_paid + commission_bought_total
+total_sold = price_sold + commission_sold_total
+
+
+profit = total_sold - total_paid
+
+
+print(f"Price paid: ${price_paid:,.2f}")
+print(f"Commission paid: ${commission_bought_total:,.2f}")
+print(f"Sell amount: ${price_sold:,.2f}")
+print(f"Commission paid: ${commission_sold_total:,.2f}")
+
+print(f"Your profit is: ${profit:,.2f}")
