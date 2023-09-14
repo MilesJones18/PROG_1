47a05666fb6132ebdbc45b3db8edf64cfdadac75
MilesJones18 <gameghostjones@gmail.com>
2023-09-13 20:01:12 -0500

    Interest project

---
+++ threadingprac.py
@@ -0,0 +1,29 @@
+import threading
+import time
+
+start = time.time()
+def print_cube(num):
+    print("Cube: {}".format(num * num * num))
+
+
+def print_square(num):
+    print("Square: {}".format(num * num))
+
+
+if __name__ == "__main__":
+    t1 = threading.Thread(target=print_cube, args=(10,))
+    t2 = threading.Thread(target=print_square, args=(10,))
+
+    t1.start()
+    t2.start()
+
+
+    t1.join()
+    t2.join()
+
+end = time.time()
+
+totaltime = end - start
+
+print(f"Done \n")
+print(f"Total Time: {totaltime}")
