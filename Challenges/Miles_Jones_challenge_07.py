95300868259140f5dff3f5245eaf05782cc0bdec
MilesJones18 <gameghostjones@gmail.com>
2023-10-03 14:42:11 -0500

    Challenge 7

---
+++ Challenges/Miles_Jones_challenge_07.py
@@ -0,0 +1,42 @@
+
+
+
+def stored_passwords(passCheck):
+    badPass = ('123456','123456789','12345','qwerty',
+               'password','12345678','111111','123123',
+               '1234567890','1234567','qwerty123','000000',
+               '1q2w3e','aa12345678','abc123','password1',
+               '1234','qwertyuiop','123321','password123',
+               '1q2w3e4r5t','iloveyou','654321','666666',
+               '987654321','123','123456a','qwe123',
+               '1q2w3e4r','7777777','1qaz2wsx','123qwe',
+               'zxcvbnm','121212','asdasd','a123456',
+               '555555','dragon','112233','123123123',
+               'monkey','11111111','qazwsx','159753',
+               'asdfghjkl','222222','1234qwer','qwerty1',
+               '123654','123abc')
+
+
+    if passCheck in badPass:
+        found = "This is a weak password"
+        return found
+    else:
+        notFound = "This is a strong password"
+        return notFound
+
+        
+
+def getUserPass():
+    userName = input("Please enter a username: ")
+    userPass = input("Please enter a password: ")
+    result = stored_passwords(userPass)
+    print(result)
+    
+
+
+def main():
+    getUserPass()
+
+
+if __name__ == "__main__":
+    main()
--- CourseProject_RPG.py
+++ CourseProject_RPG.py
@@ -7,18 +7,48 @@ import random
 
 class Humanoids:
     def __init__(self):
-        self.Hgt = input("Please enter your height: ")
-        self.Wgt = input("Please enter your weight: ")
-        self.Hclr = input("Please enter your hair color: ")
-        self.Eclr = input("Please enter your eye color: "),
+        self.Race = input("Please choose a race: ")
+        if self.Race == '1':
+            self.Race = 'Human'
+        if self.Race == '2':
+            self.Race = 'Elf'
+        if self.Race == '3':
+            self.Race = 'Dwarf'
+        self.Hgt = input(f"Please enter your {self.Race}'s height from 4ft - 7ft: ")
+        self.Wgt = input(f"Please enter your {self.Race}'s weight from 60 - 300lbs: ")
+        self.Hclr = input(f"Please enter your {self.Race}'s hair color: ")
+        self.Eclr = input(f"Please enter your {self.Race}'s eye color: ")
+        self.Str = random.randint(1,18)
+        self.Dex = random.randint(1,18)
+        self.Cons = random.randint(1,18)
+        self.Int = random.randint(1,18)
+        self.Wis = random.randint(1,18)
+        self.Char = random.randint(1,18)
+
 
     def displayCurrent(self):
-        print(f"Height: {self.Hgt}\nWeight: {self.Wgt}\nHair Color: {self.Hclr}\nEye color: {self.Eclr}\n")
+        print(f"\nRace: {self.Race}\nHeight: {self.Hgt} ft\nWeight: {self.Wgt} lbs\nHair Color: {self.Hclr}\nEye color: {self.Eclr}\n")
 
 
-class Humans(Humanoids):
-    pass
+    def displayStats(self):
+        print(f"---Attributes---\nStrength: {self.Str}\nDexterity: {self.Dex}\nConstitution: {self.Cons}\nIntelligence: {self.Int}\nWisdom: {self.Wis}\nCharisma: {self.Char}\n")
 
+
+class Humans(Humanoids):    
+    def HumanStat(self):
+        userInput = input("As a human you may add +2 to an attribute of your choice (Enter: strength, dexterity, constitution, etc)")
+        if userInput.lower == 'strength':
+            self.Str += 2
+        if userInput.lower == 'dexterity':
+            self.Dex += 2
+        if userInput.lower == 'constitution':
+            self.Cons += 2
+        if userInput.lower == 'intelligence':
+            self.Int += 2
+        if userInput.lower == 'wisdom':
+            self.Wis += 2
+        if userInput.lower == 'charisma':
+            self.Char += 2
 
 
 class Elves(Humanoids):
@@ -32,9 +62,13 @@ class Dwarves(Humanoids):
 
 
 def characterCreation():
-    print(f"Welcome to character creation!\n")
+    print(f"Welcome to character creation!\nYou may choose 1 of 3 playable races:\n1. Human\n2. Elf\n3. Dwarf\n")
     character = Humanoids()
+    if character.Race == 'Human':
+        character = Humans.HumanStat()
     character.displayCurrent()
+    character.displayStats()
+
                
 
 
