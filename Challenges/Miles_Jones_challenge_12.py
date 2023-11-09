031218d5dd6c2e340a5e49d81c114290cc465ee3
MilesJones18 <gameghostjones@gmail.com>
2023-11-08 22:03:40 -0600

    Finished challenge 12

---
+++ Challenges/Miles_Jones_challenge_12.py
@@ -0,0 +1,22 @@
+'''
+Accepts an integer and returns the sum of all the integers from 1 up to the number passed as an argument (1...N)
+where N is the number. CAN ONLY USE RECURSION
+
+Test: Use a number less than 100 and greater than 0.
+'''
+
+
+def sumAll(n):
+    #base case
+    if n == 1:
+        return 1
+
+    #recursion
+    else:
+        return n + sumAll(n-1)
+
+
+if __name__ == '__main__':
+    input = int(input('Please enter a positive number: '))
+    result = sumAll(input)
+    print(result)
